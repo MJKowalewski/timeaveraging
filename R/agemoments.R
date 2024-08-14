74291496f673a934c69696796b710253968f7621
Michal Kowalewski <kowalewski@ufl.edu>
2024-08-14 09:11:04 -0400

    update age moments and dependencies

--- R/agemoments.R
+++ R/agemoments.R
@@ -12,23 +12,30 @@
 #' @return A vector containing the following components:
 #'   \item{mean}{arithmetic mean age of specimens in a sample}
 #'   \item{median}{median age of specimens in a sample}
-#'   \item{std.dev}{standard deviation of specimen ages in a sample}
+#'   \item{sdev}{standard deviation of specimen ages in a sample}
 #'   \item{iqr}{interquartile age range of specimens in a sample}
 #'   \item{g1}{sample skewness estimate g1 of specimens in a sample}
+#'   \item{t3}{sample L-skewness estimate t3}
 #'
 #' @examples
 #'
 #' agemoments(bahamas$spec$carbonateAge)
 #'
+#' @importFrom lmom samlmu
+#'
 #' @export
 
 agemoments <- function(age) {
+  if (is.numeric(age) == FALSE) stop('age must be numeric')
+  if (is.vector(age) == FALSE) stop('age must be a vector')
+  if (sum(is.na(age)) > 0) stop('missing values not allowed')
   meanage <- mean(age)
   medianage <- stats::median(age)
   std <- stats::sd(age)
   iqr <- stats::IQR(age)
   m3 <- sum((age-mean(age))^3)/length(age)
   s3 <- stats::var(age)^(3/2)
+  t3 <- lmom::samlmu(age)[3]
   return(c(mean = meanage, median = medianage,
-           std.dev = std, iqr = iqr, g1 = m3/s3))
+           sdev = std, iqr = iqr, g1 = m3/s3, t3 = t3))
 }
--- R/timeaveraging.R
+++ R/timeaveraging.R
@@ -1,11 +1,10 @@
-#' timeaveraging: assessing age distributions of individually dated fossil specimens
+#' timeaveraging: assessing age distributions of fossil specimens
 #'
-#' The package 'timeaveraging' provides common measures of age distributions
-#' while taking in consideration dating uncertainty estimated via
+#' The package 'timeaveraging' provides functions to assess and visualize
+#' age distributions while inorporating dating uncertainty estimated via
 #' Bayesian posterior calibrations (specimen-level age probability
 #' distributions)
 #'
-#' Plot functions to visualize age distributions are also provided.
 #'
 #' The current version of the package provides functions for
 #' assessing moments of age distributions
@@ -24,4 +23,5 @@
 #' # agemoments
 #' agemoments(bahamas$spec$carbonateAge)
 #'
+#' @importFrom lmom samlmu
 NULL
